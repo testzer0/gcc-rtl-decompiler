dca99aa8f08dda350e97eac666dc92d6bc48164e
testzer0 <zer0.h6ck@gmail.com>
2020-10-25 15:13:56 +0530

    Structs done

--- ast.cc
+++ ast.cc
@@ -13,6 +13,15 @@ Node::Node() {
     parent = NULL;
 }
 
+void Node::Print(int indentlevel, const char *label) {
+    if (location) 
+        printf("%*d", 3, location->first_line);
+    else 
+        printf("%*s",3,"");
+    printf("%*s%s%s", indentlevel*3, label? label:"",GetPrintNameForNode());
+    PrintChildren(indentlevel);
+}
+
 Program::Program(List<FuncBodyList *> *bodylist) {
     Assert(bodylist != NULL);
     (funcbodylist = bodylist)->SetParentAll(this); // Set the parent pointers of the fn bodies
@@ -23,11 +32,296 @@ void Program::PrintChildren(int indentlevel) {
     printf("\n");
 }
 
-void Node::Print(int indentlevel, const char *label) {
-    if (location) 
-        printf("%*d", 3, location->first_line);
-    else 
-        printf("%*s",3,"");
-    printf("%*s%s%s", indentlevel*3, label? label:"",GetPrintNameForNode());
-    PrintChildren(indentlevel);
+FuncBody::FuncBody(StmtList *slist, char *n) {
+    Assert(slist != NULL && n != NULL);
+    (stmtlist = slist)->SetParent(this);
+    strcpy(name, n);   
 }
+
+void FuncBody::PrintChildren(int indentlevel) {
+    stmtlist->Print(indentlevel+1);
+    printf("%s\n",name);
+} 
+
+StmtList::StmtList(List<Stmt *> *ss) {
+    Assert(ss != NULL);
+    (stmts = ss)->SetParentAll(this);
+}
+
+Integer::Integer(int val) {
+    value = val;
+}
+
+void Integer::PrintChildren(int indentlevel) {
+    printf("%d",value);
+}
+
+Insn::Insn(MainCmd *mc) {
+    Assert(mc != NULL);
+    (maincmd = mc)->SetParent(this);
+}
+
+void Insn::PrintChildren(int indentlevel) {
+    maincmd->Print(indentlevel+1);
+}
+
+CmdList::CmdList(List<PlainCmd *> *cs) {
+    Assert(cs != NULL);
+    (cmds = cs)->SetParentAll(this);
+}
+
+void CmdList::PrintChildren(int indentlevel) {
+    cmds->PrintAll(indentlevel+1);
+}
+
+SetCmd::SetCmd(Operand *o1, Operand *o2) {
+    Assert(o1 != NULL && o2 != NULL);
+    (op1 = o1)->SetParent(this);
+    (op2 = o2)->SetParent(this);
+}
+
+void SetCmd::PrintChildren(int indentlevel) {
+    op1->Print(indentlevel+1);
+    op2->Print(indentlevel+1);
+}
+
+IntOperand::IntOperand(int iv) {
+    intvalue = iv;
+}
+
+void IntOperand::PrintChildren(int indentlevel) {
+    printf("%d",intvalue);
+}
+
+ExprOperand::ExprOperand(LocInfo *li, TypeInfo *ti, Expr *e) {
+    Assert(li != NULL && ti != NULL && e != NULL);
+    (linfo = li)->SetParent(this);
+    (tinfo = ti)->SetParent(this);
+    (expr = e)->SetParent(this);
+}
+
+void ExprOperand::PrintChildren(int indentlevel) {
+    linfo->Print(indentlevel+1);
+    tinfo->Print(indentlevel+1);
+    expr->Print(indentlevel+1);
+}
+
+ExtendOperand::ExtendOperand(TypeInfo *ti, Operand *o) {
+    Assert(ti != NULL && o != NULL);
+    (tinfo = ti)->SetParent(this);
+    (op = o)->SetParent(this);
+}
+
+void ExtendOperand::PrintChildren(int indentlevel) {
+    tinfo->Print(indentlevel+1);
+    op->Print(indentlevel+1);
+}
+
+TypeInfo::TypeInfo(const char *t) {
+    type = t;
+}
+
+void TypeInfo::PrintChildren(int indentlevel) {
+    printf("%s",type);
+}
+
+LocInfo::LocInfo(MemType *mt, Flags *f) {
+    Assert(mt != NULL);
+    // NOTE that f can be null
+    (mtype = mt)->SetParent(this);
+    flags = f;
+    if (flags)
+        flags->SetParent(this);
+}
+
+void LocInfo::PrintChildren(int indentlevel) {
+    mtype->Print(indentlevel+1);
+    if (flags)
+        flags->Print(indentlevel+1);
+}
+
+MemType::MemType(const char *t) {
+    type = t;
+}
+
+void MemType::PrintChildren(int indentlevel) {
+    printf("%s",type);
+}
+
+Flags::Flags(List<Flag *> *f) {
+    Assert(f != NULL);
+    // While there may be no flags for an operand, in that case the whole
+    // flaglist would be passed as NULL. Given that there is a non NULL
+    // flaglist it must not be empty
+    (flags = f)->SetParentAll(this);
+}
+
+void Flags::PrintChildren(int indentlevel) {
+    flags->PrintAll(indentlevel+1);
+}
+
+Flag::Flag(const char *f) {
+    flag = f;
+}
+
+void Flag::PrintChildren(int indentlevel) {
+    printf("%s",flag);
+}
+
+IntegerExpr::IntegerExpr(int v) {
+    value = v;
+}
+
+void IntegerExpr::PrintChildren(int indentlevel) {
+    printf("%d",value);
+}
+
+PlusExpr::PlusExpr(TypeInfo *ti, Operand *o1, Operand *o2) {
+    Assert(ti != NULL && o1 != NULL && o2 != NULL);
+    (tinfo = ti)->SetParent(this);
+    (op1 = o1)->SetParent(this);
+    (op2 = o2)->SetParent(this);
+} 
+
+void PlusExpr::PrintChildren(int indentlevel) {
+    tinfo->Print(indentlevel+1);
+    op1->Print(indentlevel+1);
+    op2->Print(indentlevel+1);
+}
+
+MinusExpr::MinusExpr(TypeInfo *ti, Operand *o1, Operand *o2) {
+    Assert(ti != NULL && o1 != NULL && o2 != NULL);
+    (tinfo = ti)->SetParent(this);
+    (op1 = o1)->SetParent(this);
+    (op2 = o2)->SetParent(this);
+}
+
+void MinusExpr::PrintChildren(int indentlevel) {
+    tinfo->Print(indentlevel+1);
+    op1->Print(indentlevel+1);
+    op2->Print(indentlevel+1);
+}
+
+MultExpr::MultExpr(TypeInfo *ti, Operand *o1, Operand *o2) {
+    Assert(ti != NULL && o1 != NULL && o2 != NULL);
+    (tinfo = ti)->SetParent(this);
+    (op1 = o1)->SetParent(this);
+    (op2 = o2)->SetParent(this);
+}
+
+void MultExpr::PrintChildren(int indentlevel) {
+    tinfo->Print(indentlevel+1);
+    op1->Print(indentlevel+1);
+    op2->Print(indentlevel+1);
+}
+
+AshiftExpr::AshiftExpr(TypeInfo *ti, Operand *o1, Operand *o2) {
+    Assert(ti != NULL && o1 != NULL && o2 != NULL);
+    (tinfo = ti)->SetParent(this);
+    (op1 = o1)->SetParent(this);
+    (op2 = o2)->SetParent(this);
+}
+
+void AshiftExpr::PrintChildren(int indentlevel) {
+    tinfo->Print(indentlevel+1);
+    op1->Print(indentlevel+1);
+    op2->Print(indentlevel+1);
+}
+
+SubregExpr::SubregExpr(TypeInfo *ti, Operand *o) {
+    Assert(ti != NULL && o != NULL);
+    (tinfo = ti)->SetParent(this);
+    (op = o)->SetParent(this);
+}
+
+void SubregExpr::PrintChildren(int indentlevel) {
+    tinfo->Print(indentlevel+1);
+    op->Print(indentlevel+1);
+}
+
+CompareExpr::CompareExpr(TypeInfo *ti, Operand *o1, Operand *o2) {
+    Assert(ti != NULL && o1 != NULL && o2 != NULL);
+    (tinfo = ti)->SetParent(this);
+    (op1 = o1)->SetParent(this);
+    (op2 = o2)->SetParent(this);
+}
+
+void CompareExpr::PrintChildren(int indentlevel) {
+    tinfo->Print(indentlevel+1);
+    op1->Print(indentlevel+1);
+    op2->Print(indentlevel+1);
+}
+
+JumpInsn::JumpInsn(Dest *d) {
+    Assert(d != NULL);
+    (dest = d)->SetParent(this);
+}
+
+void JumpInsn::PrintChildren(int indentlevel) {
+    dest->Print(indentlevel+1);
+}
+
+Label::Label(int lno) {
+    labelno = lno;
+}
+
+void Label::PrintChildren(int indentlevel) {
+    printf("%d", labelno);
+}
+
+IfThenElse::IfThenElse(Comparison *c, Dest *d1, Dest *d2) {
+    Assert(c != NULL && d1 != NULL && d2 != NULL);
+    (comp = c)->SetParent(this);
+    (dest1 = d1)->SetParent(this);
+    (dest2 = d2)->SetParent(this);
+}
+
+void IfThenElse::PrintChildren(int indentlevel) {
+    comp->Print(indentlevel+1);
+    dest1->Print(indentlevel+1);
+    dest2->Print(indentlevel+1);
+}
+
+Comparison::Comparison(Condition *c, Operand *o1, Operand *o2) {
+    Assert(c != NULL && o1 != NULL && o2 != NULL);
+    (cond = c)->SetParent(this);
+    (op1 = o1)->SetParent(this);
+    (op2 = o2)->SetParent(this);
+}
+
+void Comparison::PrintChildren(int indentlevel) {
+    cond->Print(indentlevel+1);
+    op1->Print(indentlevel+1);
+    op2->Print(indentlevel+1);
+}
+
+Condition::Condition(const char *c) {
+    cond = c;
+}
+
+void Condition::PrintChildren(int indentlevel) {
+    printf("%s",cond);
+}
+
+RetCall::RetCall(TypeInfo *ti, int rr, char *fn) {
+    Assert(ti != NULL && fn != NULL);
+    (tinfo = ti)->SetParent(this);
+    returnreg = rr;
+    strcpy(fnname, fn);
+}
+
+void RetCall::PrintChildren(int indentlevel) {
+    printf("%s",fnname);
+    // Return reg is always rax; type so far has always been SI.
+    // Not too important for now for debugging.
+}
+
+NoRetCall::NoRetCall(char *fn) {
+    Assert(fn != NULL);
+    strcpy(fnname,fn);
+}
+
+void NoRetCall::PrintChildren(int indentlevel) {
+    printf("%s",fnname);
+}
+
--- ast.h
+++ ast.h
@@ -51,6 +51,19 @@ class Error : public Node
 
 class FuncBodyList;
 class FuncBody;
+class StmtList;
+class Stmt;
+class MainCmd;
+class Operand;
+class LocInfo;
+class TypeInfo;
+class Expr;
+class MemType;
+class Flags;
+class Flag;
+class Dest;
+class Comparison;
+class Condition;
 
 class Program : public Node {
     protected:
@@ -64,7 +77,437 @@ class Program : public Node {
 };
 
 class FuncBody : public Node {
-    
-}
+    protected:
+        StmtList *stmtlist;
+        char *name;
+    public :
+        FuncBody(StmtList *slist, char *n);
+        const char *GetPrintNameForNode() {
+            return "FuncBody";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class StmtList : public Node {
+    protected:
+        List<Stmt *> *stmts;
+    public:
+        StmtList(List<Stmt *> *ss);
+        const char *GetPrintNameForNode() {
+            return "StmtList";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class Stmt : public Node {
+    public:
+        Stmt() : Node() {}
+        Stmt(yyltype loc) : Node(loc) {}
+};
+
+class Note : public Stmt {
+    public :
+        Note() : Stmt() {}
+        Note(yyltype loc) : Stmt(loc) {}        // We don't care about the notes
+        const char *GetPrintNameForNode() {
+            return "Note";
+        }
+        void PrintChildren(int indentlevel) {}
+};
+
+class Barrier : public Stmt {
+    public:
+        Barrier() : Stmt() {}
+        Barrier(yyltype loc) : Stmt() {}
+        const char *GetPrintNameForNode() {
+            return "Barrier";
+        }
+        void PrintChildren(int indentlevel) {}
+};
+
+class Integer : public Node {
+    protected:
+        int value;
+    public:
+        Integer(int val);   
+        const char *GetPrintNameForNode() {
+            return "Integer";
+        }
+        // The difference of Integer from T_IntConstant is that Integer also allows negative integers
+        // If the integer is negative then remember to pass -yylval.intconstant instead of +
+        void PrintChildren(int indentlevel);
+};
+
+
+class Insn : public Stmt {
+    protected:
+        MainCmd *maincmd;
+    public:
+        Insn(MainCmd *mc);
+        const char *GetPrintNameForNode() {
+            return "Insn";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class MainCmd : public Node {
+    public:
+        MainCmd() : Node() {}
+        MainCmd(yyltype loc) : Node(loc) {}
+        // MainCmd is of two types so we define nothing here
+};
+
+class PlainCmd : public MainCmd {
+    public:
+        PlainCmd() : MainCmd() {}
+        PlainCmd(yyltype loc) : MainCmd(loc) {}
+        // Again, PlainCmd is of three types so we define nothing here
+};
+
+class CmdList : public MainCmd {
+    protected:
+        List<PlainCmd *> *cmds;
+    public:
+        CmdList(List<PlainCmd *> *cs);
+        const char *GetPrintNameForNode() {
+            return "CmdList";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class ClobberCmd : public PlainCmd {
+    public:
+        ClobberCmd() : PlainCmd() {}
+        ClobberCmd(yyltype loc) : PlainCmd(loc) {}
+        // Don't care
+        const char *GetPrintNameForNode() {
+            return "ClobberCmd";
+        }
+        void PrintChildren(int indentlevel) {}
+};
+
+class SetCmd : public PlainCmd {
+    protected:
+        Operand *op1, *op2;
+    public:
+        SetCmd(Operand *o1, Operand *o2);
+        const char *GetPrintNameForNode() {
+            return "SetCmd";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class UseCmd : public PlainCmd {
+    public:
+        UseCmd() : PlainCmd() {}
+        UseCmd(yyltype loc) : PlainCmd(loc) {}
+        const char *GetPrintNameForNode() {
+            return "UseCmd";
+        }
+        void PrintChildren(int indentlevel) {}
+};
+
+class Operand : public Node {
+    public:
+        Operand() : Node() {}
+        Operand(yyltype loc) : Node(loc) {}
+        // There are 3 possibilities for operand, as below
+};
+
+class IntOperand : public Operand {
+    protected:
+        int intvalue;
+    public:
+        IntOperand(int iv);
+        const char *GetPrintNameForNode() {
+            return "IntOperand";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class ExprOperand : public Operand {
+    protected:
+        LocInfo *linfo;
+        TypeInfo *tinfo;
+        Expr *expr;
+    public:
+        ExprOperand(LocInfo *li, TypeInfo *ti, Expr *e);
+        const char *GetPrintNameForNode() {
+            return "ExprOperand";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class ExtendOperand : public Operand {
+    protected:
+        TypeInfo *tinfo;
+        Operand *op;
+    public:
+        ExtendOperand(TypeInfo *ti, Operand *o);
+        const char *GetPrintNameForNode() {
+            return "ExtendOperand";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class TypeInfo : public Node {
+    protected:
+        const char *type;
+    public:
+        TypeInfo(const char *t);
+        const char *GetPrintNameForNode() {
+            return "TypeInfo";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class LocInfo : public Node {
+    protected:
+        MemType *mtype;
+        Flags *flags;
+    public:
+        LocInfo(MemType *mt, Flags *f);
+        const char *GetPrintNameForNode() {
+            return "LocInfo";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class MemType : public Node {
+    protected:
+        const char *type;
+    public:
+        MemType(const char *t);
+        const char *GetPrintNameForNode() {
+            return "MemType";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class Flags : public Node {
+    protected:
+        List<Flag *> *flags;
+    public:
+        Flags(List<Flag *> *f);
+        const char *GetPrintNameForNode() {
+            return "Flags";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class Flag : public Node {
+    protected:
+        const char *flag;
+    public:
+        Flag(const char *f);
+        const char *GetPrintNameForNode() {
+            return "Flag";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class Expr : public Node {
+    public:
+        Expr() : Node() {}
+        Expr(yyltype loc) : Node(loc) {}
+        // Expr is of 7 types
+};
+
+class IntegerExpr : public Expr {
+    protected:
+        int value;
+    public:
+        IntegerExpr(int v);
+        const char *GetPrintNameForNode() {
+            return "IntegerExpr";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class PlusExpr : public Expr {
+    protected:
+        TypeInfo *tinfo;
+        Operand *op1;
+        Operand *op2;
+    public:
+        PlusExpr(TypeInfo *ti, Operand *o1, Operand *o2);
+        const char *GetPrintNameForNode() {
+            return "PlusExpr";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class MinusExpr : public Expr {
+    protected:
+        TypeInfo *tinfo;
+        Operand *op1;
+        Operand *op2;
+    public:
+        MinusExpr(TypeInfo *ti, Operand *o1, Operand *o2);
+        const char *GetPrintNameForNode() {
+            return "MinusExpr";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class MultExpr : public Expr {
+    protected:
+        TypeInfo *tinfo;
+        Operand *op1;
+        Operand *op2;
+    public:
+        MultExpr(TypeInfo *ti, Operand *o1, Operand *o2);
+        const char *GetPrintNameForNode() {
+            return "MultExpr";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class AshiftExpr : public Expr {
+    protected:
+        TypeInfo *tinfo;
+        Operand *op1;
+        Operand *op2;
+    public:
+        AshiftExpr(TypeInfo *ti, Operand *o1, Operand *o2);
+        const char *GetPrintNameForNode() {
+            return "AshiftExpr";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class SubregExpr : public Expr {
+    protected:
+        TypeInfo *tinfo;
+        Operand *op;
+    public:
+        SubregExpr(TypeInfo *ti, Operand *o);
+        const char *GetPrintNameForNode() {
+            return "SubregExpr";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class CompareExpr : public Expr {
+    protected:
+        TypeInfo *tinfo;
+        Operand *op1;
+        Operand *op2;;
+    public:
+        CompareExpr(TypeInfo *ti, Operand *o1, Operand *o2);
+        const char *GetPrintNameForNode() {
+            return "CompareExpr";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class JumpInsn : public Stmt {
+    protected:
+        Dest *dest;
+    public:
+        JumpInsn(Dest *d);
+        const char *GetPrintNameForNode() {
+            return "JumpInsn";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class Dest : public Node {
+    public:
+        Dest() : Node() {}
+        Dest(yyltype loc) : Node(loc) {}
+        // Dest is also of two types, label or if_then_else
+};
+
+class Label : Dest {
+    protected:
+        int labelno;
+    public:
+        Label(int lno);
+        const char *GetPrintNameForNode() {
+            return "Label";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class Pc : Dest {
+    // Not actually a possible production. However since we need Dest
+    // to double up as InDest as well, we will let Pc derive from Dest.
+    public:
+        Pc() : Dest() {}
+        Pc(yyltype loc) : Dest(loc) {}
+        const char *GetPrintNameForNode() {
+            return "Pc";
+        }
+        void PrintChildren() {}
+};
+
+class IfThenElse : Dest {
+    protected:
+        Comparison *comp;
+        Dest *dest1;
+        Dest *dest2; // these should be InDests actually, but Dest will do.
+    public:
+        IfThenElse(Comparison *c, Dest *d1, Dest *d2);
+        const char *GetPrintNameForNode() {
+            return "IfThenElse";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class Comparison : public Node {
+    protected:
+        Condition *cond;
+        Operand *op1;
+        Operand *op2;
+    public:
+        Comparison(Condition *c, Operand *o1, Operand *o2);
+        const char *GetPrintNameForNode() {
+            return "Comparison";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class Condition : public Node {
+    protected:
+        const char *cond;
+    public:
+        Condition(const char *c);
+        const char *GetPrintNameForNode() {
+            return "Condition";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class Call : public Stmt {
+    public:
+        Call() : Stmt() {}
+        Call(yyltype loc) : Stmt(loc) {}
+        // Call is of two types
+};
+
+class RetCall : public Call {
+    protected:
+        TypeInfo *tinfo;
+        int returnreg;
+        char *fnname;
+    public:
+        RetCall(TypeInfo *ti, int rr, char *fn);
+        const char *GetPrintNameForNode() {
+            return "RetCall";
+        }
+        void PrintChildren(int indentlevel);
+};
+
+class NoRetCall : public Call {
+    protected:
+        char *fnname;
+    public:
+        NoRetCall(char *fn);
+        const char *GetPrintNameForNode() {
+            return "NoRetCall";
+        }
+        void PrintChildren(int indentlevel);
+};
 
 #endif
